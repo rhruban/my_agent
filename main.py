ac0ff7755be518838804d481f1f794f177aa5962
rhruban <rhruban@gmail.com>
2025-06-29 08:10:11 -0600

    added main.py with simple prompting

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=content
+)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
