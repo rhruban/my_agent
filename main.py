7d623f59875fb3c071b3ed54656b9343ded50894
rhruban <rhruban@gmail.com>
2025-07-01 10:53:59 -0600

    added message storage for converations

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import sys
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 if len(sys.argv) != 2:
     print("Error: Include prompt")
     sys.exit(1)
 
-content = sys.argv[1]
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=content
+    contents=messages,
 )
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
