a5832b0c3c75ce2f95bc3fa408da512a37e464ed
rhruban <rhruban@gmail.com>
2025-07-02 14:03:46 -0600

    added system prompt ability

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
@@ -35,6 +37,7 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     print("Response:")
     print(response.text)
